ec7c6a66213b22dd2c0d1131c737c38915c1a513
joemar25 <{ID}+{username}@users.noreply.github.com>
2023-01-27 18:46:20 +0800

    updated version before installing new packages

--- app/server.py
+++ app/server.py
@@ -14,25 +14,20 @@ app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 
 class Routes:
 
-    @app.route("/")
+    @app.route("/", methods=['GET', 'POST'])
+    @app.route("/index", methods=['GET', 'POST'])
     def index():
         """
             #### Index function in route ('/')
             - is the first page that the user will see
             - user can enter their name and the script for their speech
         """
-        return render_template('index.html')
+        text = ""
+        user_name = ""
+        upload = ""
 
-    @app.route("/index", methods=['GET', 'POST'])
-    def index_test():
-        """
-            #### FOR TESTING
-            - get the requested input and return it on same page ('/index')
-        """
         if request.method == "POST":
-
             user_name = request.form['username']
-
             """
                 - create a validation tester for:
                   text is too short
@@ -42,10 +37,8 @@ class Routes:
             upload = request.files['file_script']
 
             if text == "" and upload.filename != '':
-                upload.save(UPLOAD_FOLDER,
-                            secure_filename(upload.filename))
+                upload.save(UPLOAD_FOLDER, secure_filename(upload.filename))
             else:
-                # return render_template("index.html")
                 return render_template("index.html", user_name=user_name, text_script=text)
         return render_template("index.html", user_name=user_name, text_script=text)
 
@@ -81,16 +74,3 @@ class Routes:
 if __name__ == "__main__":
     # we are in development mode
     app.run(debug=True, threaded=True, host='localhost', port=8080)
-
-
-# @app.route('/form')
-# def form():
-#     return render_template('form.html')
-
-
-# @app.route('/upload', methods=['POST', 'GET'])
-# def upload():
-#     if request.method == 'POST':
-#         f = request.files['file']
-#         f.save(secure_filename(f.filename))
-#         return "File saved successfully"
