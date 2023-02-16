74258a15bb73fe2b3c2fb9be51e314371de09258
Olan Gomez <johnolansaldivar.gomez@bicol-u.edu.ph>
2023-02-16 22:56:29 +0800

    1. Added Grammar class - gingerit_class.py 2. Application of the Grammar class - grammarTest.py 3. Tried testing the accuracy of the Grammar class with 3 texts. Results are in "testing of grammar accuracy.txt" 4. Added code development documentation

---
+++ project/scripts/grammar/gingerit_class.py
@@ -0,0 +1,39 @@
+# John Olan S. Gomez
+# SE2 Final project
+# February 16, 2023
+
+from gingerit.gingerit import GingerIt as gingerit  #checks grammar
+import nltk.data    #separates sentences
+nltk.download('punkt')  #you can comment this out, once it is already downloaded
+
+class Grammar:
+
+    # Steps:
+    # get the paragraph
+    # separate the paragraph into sentences
+    # correct the grammar in each sentence
+    # recombine the sentences
+
+    def __init__(self, sentence):
+        self.sentence = sentence
+        self.sentences = []
+
+    def separateParagraph(self):
+        tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+        data = self.sentence
+        self.sentences.append(tokenizer.tokenize(data))
+
+    def checkGrammar(self):
+        Grammar.separateParagraph(self) # separate the sentences
+        correctSentence = []    # array to hold the corrected sentences
+        parser = gingerit()     # parse gingerit
+
+        # loop through the sentences one by one
+        for i in self.sentences:
+            for j in i:
+                result = parser.parse(j)
+                correctSentence.append(result['result'])
+
+        complete = " ".join(correctSentence)    # join the corrected sentences
+
+        return complete
