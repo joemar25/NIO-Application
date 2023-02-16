02074e24f7376cc877516eca6c90ebcc12cdecdd
joemar25 <{ID}+{username}@users.noreply.github.com>
2023-02-16 20:19:30 +0800

    Optimizing

---
+++ project/static/js/index.js
@@ -0,0 +1,10 @@
+/*
+    NOTE:
+        run this command on project's folder: browserify static/js/index.js -o static/js/bundle.js
+
+        - but can do: 'npm run bfy' since i (Joemar) modify the command inside package.json
+*/
+
+// include the script file, in this same folder
+require('./script.js');
+
