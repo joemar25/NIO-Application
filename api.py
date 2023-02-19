efc4d6c551429cc6d52d9bae9673301c7e1c08f1
joemar25 <{ID}+{username}@users.noreply.github.com>
2023-02-19 09:32:05 +0800

    Main AAA

--- api.py
+++ api.py
@@ -7,4 +7,5 @@ if __name__ == "__main__":
         configure package.json and set the to "proxy": "http://127.0.0.1:8080" since we use
         8080 else just put the "proxy": "http://127.0.0.1:5000" since it is the default proxy
     """
+    
     app.run(debug=True, threaded=True, host='localhost', port=8080)
