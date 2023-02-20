6cbfa95513b8c4f64f956f37a4a3c986788efca1
joemar25 <joemar.cardino000@gmail.com>
2023-02-20 19:18:38 +0800

    feedback updated file + charts

--- project/static/js/index.js
+++ project/static/js/index.js
@@ -1,12 +1,13 @@
 /*
 NOTE:
 run this command on project's folder: browserify static/js/index.js -o static/js/bundle.js
-- just run: 'npm run bfy' since i (Joemar) modify the command inside package.json
+- just run: 'npm run bfy' since (Joemar) modify the command inside package.json
 */
 
 // important requirements from node_module
 require('flowbite')
-// include recording.js
+// include local scripts
 require('./recording.js');
+require('./chart.js');
 // include the script file, in this same folder
 require('./script.js');
