19a3afcec9449f4a2182d884848f09f1898b0844
joemar25 <joemar.cardino000@gmail.com>
2023-02-27 17:30:42 +0800

    Update

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-__version__ = "0.8"
+__version__ = "0.9"
 __docformat__ = "restructuredtext en"
 
 from project import app
--- project/scripts/rate.py
+++ project/scripts/rate.py
@@ -1,59 +1,72 @@
 # John Olan S. Gomez
+# Joemar
 # Finding the rate of the speech
-# 12-05-2022
+# 02-27-2023
 
-FILE = r"C:\Users\Olan\Documents\Olan\3rd_year\CS_117-Software_Engineering_1\Prototype\rate\venv\happy_olan.wav"
-LOWEST_IDEAL = 2.33
-HIGHEST_IDEAL = 2.67
-REDUCTION = 0.0683 / 0.17
-IDEAL_SCORE = 100
-
+import os
 import whisper
 import librosa
 
-model = whisper.load_model('base')
+def get_rate(audio_file) : 
+    
+    LOWEST_IDEAL = 2.33
+    HIGHEST_IDEAL = 2.67
+    REDUCTION = 0.0683 / 0.17
+    IDEAL_SCORE = 100
 
-result = model.transcribe(
-    FILE,
-    fp16=False,
-    language='English',
-    task='Translate'
-)
+    model = whisper.load_model('base')
 
-audioDuration= float(librosa.get_duration(filename=(FILE)))
-sentence= str(result["text"])
-wordList= str.split(sentence)
-wordNum= int(len(wordList))
-rate= wordNum/audioDuration
-wpm= rate*60
+    result = model.transcribe(
+        audio_file,
+        fp16=False,
+        language='English',
+        task='Translate'
+    )
 
-# calculate speech score
-if rate<LOWEST_IDEAL:
-    speechScore= LOWEST_IDEAL - rate
-    speechScore= speechScore * REDUCTION
-    speechScore= IDEAL_SCORE - speechScore
-elif rate>HIGHEST_IDEAL:
-    speechScore = rate - HIGHEST_IDEAL
-    speechScore = speechScore * REDUCTION
-    speechScore = IDEAL_SCORE - speechScore
+    audioDuration= float(librosa.get_duration(filename=(audio_file)))
+    sentence= str(result["text"])
+    wordList= str.split(sentence)
+    wordNum= int(len(wordList))
+    rate= wordNum/audioDuration
+    wpm= rate*60
 
-else:
-    speechScore= IDEAL_SCORE
+    # calculate speech score
+    if rate<LOWEST_IDEAL:
+        speechScore= LOWEST_IDEAL - rate
+        speechScore= speechScore * REDUCTION
+        speechScore= IDEAL_SCORE - speechScore
+    elif rate>HIGHEST_IDEAL:
+        speechScore = rate - HIGHEST_IDEAL
+        speechScore = speechScore * REDUCTION
+        speechScore = IDEAL_SCORE - speechScore
+
+    else:
+        speechScore= IDEAL_SCORE
 
 
 
-#determine speed of the speech
-speechRating = ""
-if wpm < 140:
-    speechRating = "Slow"
-elif wpm >= 140 and wpm <=160:
-    speechRating = "Ideal"
-else:
-    speechRating = "Fast"
+    #determine speed of the speech
+    speechRating = ""
+    if wpm < 140:
+        speechRating = "Slow"
+    elif wpm >= 140 and wpm <=160:
+        speechRating = "Ideal"
+    else:
+        speechRating = "Fast"
 
-print("Audio duration: ", audioDuration)
-print("Number of words in sentence:", wordNum)
-print("Speech score: %.2f%%" % speechScore)
-print("Speech rate: %.2f wpm" % wpm)
-print("Your speech rate is:", speechRating)
-print(sentence)
+    print("Audio duration: ", audioDuration)
+    print("Number of words in sentence:", wordNum)
+    print("Speech score: %.2f%%" % speechScore)
+    print("Speech rate: %.2f wpm" % wpm)
+    print("Your speech rate is:", speechRating)
+    print(sentence)
+    
+
+audio_title = '\sample.wav' # change this audio file
+audio_path = os.getcwd() + audio_title
+print()
+print()
+print(audio_path)
+print()
+print()
+# get_rate(audio_path) # use function for testing
