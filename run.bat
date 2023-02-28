bede2ec703a595fa03601c6bb230e032f6de4f59
joemar25 <joemar.cardino000@gmail.com>
2023-02-28 22:41:32 +0800

    Typography Added and Run.bat fix

--- run.bat
+++ run.bat
@@ -18,34 +18,8 @@ pip install --upgrade --no-deps -r others/requirements.txt
 REM Change directory to the project folder
 cd /d "%~dp0..\NIO-APPLICATION\project"
 
-REM Check if required Node packages are already installed
-echo Checking if required Node packages are already installed...
-IF NOT EXIST "node_modules\autoprefixer" (
-  echo Installing autoprefixer...
-  npm install -D autoprefixer
-)
-
-IF NOT EXIST "node_modules\browserify" (
-  echo Installing browserify...
-  npm i browserify
-)
-
-IF NOT EXIST "node_modules\postcss-cli" (
-  echo Installing postcss-cli...
-  npm install -D postcss-cli
-)
-
-IF NOT EXIST "node_modules\tailwindcss" (
-  echo Installing tailwindcss...
-  npm install -D tailwindcss
-)
-
 REM Run the postcss command
-echo Running postcss...
-start cmd /c "npm run postcss"
-
-REM Run the browserify command
-echo Running browserify...
-start cmd /c "npm run bfy"
+echo Running npm...
+start cmd /c "npm install"
 
 call start.bat
