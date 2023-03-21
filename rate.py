908a947a09d74f7adbc8cd53421ca5c05d15d578
joemar25 <joemar.cardino000@gmail.com>
2023-03-21 21:14:11 +0800

    updated

--- rate.py
+++
@@ -1,7 +0,0 @@
-from project.scripts.rate import rate_score
-
-audio = "harvard.wav"
-text = "The stale smell of old beer lingers. It takes heat to bring out the odor. A cold dip restores health and zest. A salt pickle tastes fine with ham. Taco's al pastor are my favorite. A zestful food is the hot cross bun."
-
-rscore = rate_score(audio, text, use_temp_folder=False)
-print("overall: ", rscore)
--- test_trasciption.py
+++
@@ -1,5 +0,0 @@
-from project.scripts.transcribe import to_text
-
-file_name = "harvard.wav"
-transcribed = to_text(file_name, use_temp_folder=False)
-print("output: ", transcribed)
