3d8fe1b75fb9401910eb3e8b004716a32fc3cb91
joemar25 <joemar.cardino000@gmail.com>
2023-03-22 14:47:11 +0800

    added timer - recording

--- project/static/js/recording.js
+++ project/static/js/recording.js
@@ -7,7 +7,7 @@ async function startRecording() {
 
         // create media recorder
         mediaRecorder = new MediaRecorder(stream, {
-            mimeType: 'audio/webm;codecs=opus',
+            mimeType: "audio/webm;codecs=opus",
         });
 
         // handle data and stop events
@@ -15,7 +15,25 @@ async function startRecording() {
             chunks.push(data);
         });
 
+        // Set up timer
+        const startTime = Date.now();
+        const timerElement = document.getElementById("timer");
+        timerElement.classList.remove("hidden");
+
+        const updateTimer = () => {
+            const elapsedTime = new Date(Date.now() - startTime);
+            const minutes = elapsedTime.getUTCMinutes().toString().padStart(2, "0");
+            const seconds = elapsedTime.getUTCSeconds().toString().padStart(2, "0");
+            timerElement.textContent = `${minutes}:${seconds}`;
+        };
+
+        const timerInterval = setInterval(updateTimer, 1000);
+
         mediaRecorder.addEventListener("stop", async () => {
+
+            // clear time
+            clearInterval(timerInterval);
+
             // create audio blob and form data
             const audioBlob = new Blob(chunks, { type: chunks[0].type });
             const timestamp = Date.now();
@@ -30,36 +48,47 @@ async function startRecording() {
                 // send audio to server
                 const response = await fetch("/upload", {
                     method: "POST",
-                    body: formData
+                    body: formData,
                 });
 
                 // handle response
                 if (response.ok) {
                     // redirect to success page
-                    window.location.href = '/process_audio';
+                    window.location.href = "/process_audio";
                 } else {
                     // show error message
-                    console.error("Error sending audio recording to server:", response.statusText);
-                    window.location.href = '/process_audio_fail';
+                    console.error(
+                        "Error sending audio recording to server:",
+                        response.statusText
+                    );
+                    window.location.href = "/process_audio_fail";
                 }
             } catch (error) {
                 console.error("Error sending audio recording to server:", error);
-                window.location.href = '/process_audio_fail';
+                window.location.href = "/process_audio_fail";
             }
 
             // hide loading animation
             loadingContainer.classList.remove("show");
         });
 
+        // Set recording time limit
+        const recordingTimeLimit = 5 * 60 * 1000; // 5 minutes in milliseconds
+        setTimeout(() => {
+            stopRecording();
+        }, recordingTimeLimit);
+
         // start recording
         mediaRecorder.start();
     } catch (error) {
         console.error(error);
-        window.location.href = '/process_audio_fail';
+        window.location.href = "/process_audio_fail";
     }
 }
 
+
 function stopRecording() {
+
     // Stop recording and release resources
     mediaRecorder.stop();
     stream.getTracks().forEach(track => track.stop());
