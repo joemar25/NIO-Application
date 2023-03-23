5f206f8c56372249582494a2812c347069d61d79
joemar25 <joemar.cardino000@gmail.com>
2023-03-23 15:55:14 +0800

    rename main

--- project/scripts/emotion/__init__.py
+++ project/scripts/emotion/__init__.py
@@ -1,1 +1,7 @@
-from main import EmotionDetector
+from EmotionDetector import EmotionDetector
+import os
+
+detector = EmotionDetector()
+audio_path = os.getcwd() + "/project/temp_data/23322b4e7711e-c8a2-11ed-a657-708bcd015b0c111426.wav"
+predictions = detector.predict_emotion(audio_path)
+print(predictions)
