ca29ee6e8710636c14d88971fa5202006e1d4bc3
joemar25 <joemar.cardino000@gmail.com>
2023-03-23 15:41:15 +0800

    Added Emotion Detection Training and Main Class

---
+++ project/scripts/emotion/main.py
@@ -0,0 +1,57 @@
+import os, librosa, joblib, numpy as np
+from keras.models import load_model
+
+class EmotionDetector:
+    def __init__(self):
+        file_loc = os.getcwd() + "/project/scripts/emotion/"
+
+        # Load the model, scaler, and encoder
+        self.model = load_model(file_loc + "emotion_detect.h5")
+        self.scaler = joblib.load(file_loc + "scaler.pkl")
+        self.encoder = joblib.load(file_loc + "encoder.pkl")
+
+    def predict_emotion(self, audio_path):
+        data, sample_rate = librosa.load(audio_path)
+
+        # Extract features and transform with the scaler
+        features = self.scaler.transform(self.extract_features(data, sample_rate).reshape(1, -1))
+
+        # Predict the emotions
+        pred_test = self.model.predict(np.expand_dims(features, axis=2), verbose=0)[0]
+
+        # Combine into a dictionary
+        emotion_dict = dict(zip(self.encoder.categories_[0], pred_test))
+
+        # Sort emotions by confidence level
+        sorted_emotions = sorted(emotion_dict.items(), key=lambda x: x[1], reverse=True)
+
+        # Return top 3 detected emotions in an array with their confidence score
+        return [(emotion, "{:.2f}".format(score * 100)) for (emotion, score) in sorted_emotions[:3]]
+
+    def extract_features(self, data, sample_rate):
+        # ZCR
+        result = np.mean(librosa.feature.zero_crossing_rate(y=data).T, axis=0)
+
+        # Chroma_stft
+        stft = np.abs(librosa.stft(data))
+        chroma_stft = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T, axis=0)
+
+        # MFCC
+        mfcc = np.mean(librosa.feature.mfcc(y=data, sr=sample_rate).T, axis=0)
+
+        # Root Mean Square Value
+        rms = np.mean(librosa.feature.rms(y=data).T, axis=0)
+
+        # MelSpectogram
+        mel = np.mean(librosa.feature.melspectrogram(y=data, sr=sample_rate).T, axis=0)
+
+        # Return concatenate all the features
+        return np.concatenate([result, chroma_stft, mfcc, rms, mel])
+
+# with demo test
+# detector = EmotionDetector()
+# audio_path = os.getcwd() + "/project/your_wave_filename.wav"
+# predictions = detector.predict_emotion(audio_path)
+# print(predictions)
+
+
---
+++ project/scripts/emotion/model_utilization.py
@@ -0,0 +1,76 @@
+import os
+import pandas as pd
+import numpy as np
+import librosa
+import librosa.display
+from joblib import dump
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
+from sklearn.model_selection import train_test_split
+
+from keras.models import Sequential
+from keras.layers import Dense, Conv1D, MaxPooling1D, Flatten, Dropout
+
+file_loc = os.getcwd() + "/project/scripts/emotion/"
+directory = file_loc + "features.csv"
+
+Features = pd.read_csv(directory)
+
+# Data Preparation - splitting the data for training and testing
+X = Features.iloc[: ,:-1].values
+Y = Features['labels'].values
+
+# As this is a multiclass classification problem onehotencoding our Y
+encoder = OneHotEncoder()
+Y = encoder.fit_transform(np.array(Y).reshape(-1,1)).toarray()
+
+# splitting data
+x_train, x_test, y_train, y_test = train_test_split(X, Y, random_state=0, shuffle=True)
+x_train.shape, y_train.shape, x_test.shape, y_test.shape
+
+# scaling our data with sklearn's Standard scaler
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+x_train.shape, y_train.shape, x_test.shape, y_test.shape
+
+# making our data compatible to model
+x_train = np.expand_dims(x_train, axis=2)
+x_test = np.expand_dims(x_test, axis=2)
+x_train.shape, y_train.shape, x_test.shape, y_test.shape
+
+# Modelling
+model = Sequential()
+model.add(Conv1D(256, kernel_size=5, strides=1, padding='same', activation='relu', input_shape=(x_train.shape[1], 1)))
+model.add(MaxPooling1D(pool_size=5, strides = 2, padding = 'same'))
+
+model.add(Conv1D(256, kernel_size=5, strides=1, padding='same', activation='relu'))
+model.add(MaxPooling1D(pool_size=5, strides = 2, padding = 'same'))
+
+model.add(Conv1D(128, kernel_size=5, strides=1, padding='same', activation='relu'))
+model.add(MaxPooling1D(pool_size=5, strides = 2, padding = 'same'))
+model.add(Dropout(0.2))
+
+model.add(Conv1D(64, kernel_size=5, strides=1, padding='same', activation='relu'))
+model.add(MaxPooling1D(pool_size=5, strides = 2, padding = 'same'))
+
+model.add(Flatten())
+model.add(Dense(units=32, activation='relu'))
+model.add(Dropout(0.3))
+
+model.add(Dense(units=8, activation='softmax'))
+model.compile(optimizer = 'adam' , loss = 'categorical_crossentropy' , metrics = ['accuracy'])
+
+# train
+model.fit(x_train, y_train, batch_size=32, epochs=50, validation_data=(x_test, y_test))
+
+# initialize location path for saving
+file_loc = os.getcwd() + "/project/scripts/emotion/"
+model_save = file_loc + "emotion_detect.h5"
+scaler_save = file_loc + "scaler.pkl"
+encoder_save = file_loc + "encoder.pkl"
+
+# Save the scaler, encoder and model
+dump(scaler, scaler_save)
+dump(encoder, encoder_save)
+model.save(model_save)
