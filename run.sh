04197414f4ff3c03e52bafee103c89bafc3993b6
joemar25 <joemar.cardino000@gmail.com>
2023-03-26 21:09:21 +0800

    aaaa

--- run.sh
+++ run.sh
@@ -1,36 +1,16 @@
 #!/bin/bash
-
 echo "Mar-Automation..."
 
 # Upgrading pip
 echo "Upgrading pip..."
 pip install --upgrade pip
 
-# Install libries
-pip install flask
-pip install Flask-WTF
-pip install Flask-Login
-pip install Flask-SQLAlchemy
-pip install Werkzeug
-pip install Flask-SQLAlchemy
-pip install pytz
-pip install psycopg2-binary
-pip install gunicorn
-pip install nltk
-pip install librosa
-pip install pydub
-pip install openai-whisper
-pip install tensorflow
-pip install keras
-
 # Install FFmpeg
-# sudo apt update && sudo add-apt-repository universe && sudo apt update && sudo apt install ffmpeg -y
+sudo apt update && sudo add-apt-repository universe && sudo apt update && sudo apt install ffmpeg -y
 
-# Install gunicorn if not already installed
-if ! pip list | grep -q gunicorn; then
-  echo "Installing gunicorn..."
-  pip install gunicorn
-fi
+# Install required Python packages
+echo "Installing Python packages..."
+pip install -r requirements.txt
 
 # Change to the project directory
 cd project
@@ -40,4 +20,3 @@ echo "Running npm..."
 npm install
 
 cd ..
-
