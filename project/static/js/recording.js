0003a7788eee96b8cd505d7b232d4be36b420f4b
joemar25 <joemar.cardino000@gmail.com>
2023-05-07 13:36:26 +0800

    integration of recording page with new ui Fixes #38

--- project/static/js/recording.js
+++ project/static/js/recording.js
@@ -1,3 +1,19 @@
+/*
+ * This is a JavaScript class that defines a simple audio recorder. The recorder
+ * allows users to record audio from their device's microphone and upload it to
+ * a server for further processing. The recorder interface displays a timer that
+ * shows the elapsed time during recording, a countdown before recording starts,
+ * and a button to start and stop the recording.
+ *
+ * The class uses the Web Audio API and the MediaRecorder API to capture and
+ * record audio data, and the fetch() function to upload the recorded audio to
+ * a server. The recorder also includes error handling for cases where the user
+ * denies access to the microphone or the server returns an error response.
+ *
+ * This code was written for educational purposes only and should not be used in
+ * production environments without proper testing and security considerations.
+ */
+
 class Recorder {
   constructor() {
     this.mediaRecorder = null;
