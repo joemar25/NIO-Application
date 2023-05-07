79215f5e6bd140c021c21fc80b72d3aede02c727
joemar25 <joemar.cardino000@gmail.com>
2023-05-07 13:35:46 +0800

    integrated record and new ui fixes

--- project/static/js/output.js
+++ project/static/js/output.js
@@ -21667,22 +21667,6 @@ require("./recording.js");
 require("./verification.js");
 
 },{"./dark_mode.js":53,"./fab_controller.js":54,"./home_controller.js":55,"./recording.js":57,"./toast.js":58,"./verification.js":59,"blueimp-file-upload/js/jquery.fileupload":2,"flowbite":41,"flowbite-typography":4}],57:[function(require,module,exports){
-/*
- * This is a JavaScript class that defines a simple audio recorder. The recorder
- * allows users to record audio from their device's microphone and upload it to
- * a server for further processing. The recorder interface displays a timer that
- * shows the elapsed time during recording, a countdown before recording starts,
- * and a button to start and stop the recording.
- *
- * The class uses the Web Audio API and the MediaRecorder API to capture and
- * record audio data, and the fetch() function to upload the recorded audio to
- * a server. The recorder also includes error handling for cases where the user
- * denies access to the microphone or the server returns an error response.
- *
- * This code was written for educational purposes only and should not be used in
- * production environments without proper testing and security considerations.
- */
-
 class Recorder {
   constructor() {
     this.mediaRecorder = null;
@@ -21691,34 +21675,36 @@ class Recorder {
     this.remainingTime = 0;
     this.countdownIntervalId = null;
     this.recordingIntervalId = null;
+    this.timerIntervalId = null;
 
     const recordBtn = document.getElementById("record-btn");
-    // const cancelBtn = document.getElementById("cancel-btn");
     const timerDiv = document.getElementById("timer");
     const countdownDiv = document.getElementById("countdown");
+    const notif = document.getElementById("notif");
 
     recordBtn.addEventListener("click", () => {
-      const { state } = recordBtn.dataset;
-      if (state === "ready") {
-        this.startCountdown();
-      } else if (state === "recording") {
-        this.stopRecording();
-        recordBtn.value = "Ready";
-        recordBtn.disabled = true;
-      }
+      this.toggleState();
     });
-
-    // cancelBtn.addEventListener("click", () => {
-    //   this.cancelRecording();
-    //   recordBtn.value = "Ready";
-    // });
 
     Object.assign(this, {
       recordBtn,
-      // cancelBtn,
       timerDiv,
       countdownDiv,
+      notif,
     });
+  }
+
+  toggleState() {
+    const { state } = this.recordBtn.dataset;
+    if (state === "ready") {
+      this.recordBtn.dataset.state = "recording";
+      this.startCountdown();
+      this.notif.textContent = "Prepare in...";
+    } else if (state === "recording") {
+      this.recordBtn.dataset.state = "ready";
+      this.stopRecording();
+      this.notif.textContent = "Audio is now processing";
+    }
   }
 
   startCountdown() {
@@ -21740,17 +21726,10 @@ class Recorder {
   }
 
   async startRecording() {
-    // Remove the countdown and show recording UI elements
     this.showRecordingUI();
-
-    // Set up the recording timer and interval
     this.setUpRecordingTimer();
-
     try {
-      // Get user media and create media recorder
       await this.setUpMediaRecorder();
-
-      // Start the media recorder
       this.startMediaRecorder();
     } catch (error) {
       console.error(error);
@@ -21760,18 +21739,17 @@ class Recorder {
 
   showRecordingUI() {
     this.recordBtn.classList.remove("hidden");
-    this.countdownDiv.classList.add("hidden");
     this.timerDiv.classList.remove("hidden");
     this.recordBtn.value = "Stop";
     this.recordBtn.disabled = false;
     this.recordBtn.dataset.state = "recording";
-    // this.cancelBtn.style.display = "inline-block";
+    this.notif.textContent = "Tap to stop recording";
   }
 
   setUpRecordingTimer() {
     this.remainingTime = 0;
 
-    this.recordingIntervalId = setInterval(() => {
+    this.timerIntervalId = setInterval(() => {
       this.remainingTime += 1;
       const minutes = Math.floor(this.remainingTime / 60);
       const seconds = this.remainingTime % 60;
@@ -21782,15 +21760,12 @@ class Recorder {
   }
 
   async setUpMediaRecorder() {
-    // Get user media
     this.stream = await navigator.mediaDevices.getUserMedia({ audio: true });
 
-    // Create media recorder
     this.mediaRecorder = new MediaRecorder(this.stream, {
       mimeType: "audio/webm;codecs=opus",
     });
 
-    // Handle data and stop events
     this.mediaRecorder.addEventListener("dataavailable", ({ data }) => {
       this.chunks.push(data);
     });
@@ -21801,42 +21776,32 @@ class Recorder {
   }
 
   startMediaRecorder() {
-    // Set recording time limit
-    const recordingTimeLimit = 5 * 60 * 1000; // 5 minutes in milliseconds
-    setTimeout(() => {
-      this.stopMediaRecorder();
+    const recordingTimeLimit = 5 * 60 * 1000;
+    this.recordingTimeoutId = setTimeout(() => {
+      this.stopRecording();
     }, recordingTimeLimit);
-
-    // Start the media recorder
     this.mediaRecorder.start();
   }
 
   async handleMediaRecorderStop() {
-    // Clear recording timer
-    clearInterval(this.recordingIntervalId);
-    // Create audio blob and form data
+    clearInterval(this.timerIntervalId);
     const audioBlob = new Blob(this.chunks, { type: this.chunks[0].type });
     const timestamp = Date.now();
     const formData = new FormData();
     formData.append("audio", audioBlob, `${timestamp}.wav`);
 
-    // Show loading animation
     const loadingContainer = document.getElementById("loading-container");
     loadingContainer.classList.add("show");
 
     try {
-      // Send audio to server
       const response = await fetch("/upload", {
         method: "POST",
         body: formData,
       });
 
-      // Handle response
       if (response.ok) {
-        // Redirect to success page
         window.location.href = "/process_audio";
       } else {
-        // Show error message
         console.error(
           "Error sending audio recording to server:",
           response.statusText
@@ -21848,35 +21813,17 @@ class Recorder {
       window.location.href = "/process_audio_fail";
     }
 
-    // Hide loading animation
     loadingContainer.classList.remove("show");
   }
 
-  stopMediaRecorder() {
-    this.mediaRecorder.stop();
-  }
-
-  stopRecording(callback) {
-    clearInterval(this.recordingIntervalId);
+  stopRecording() {
     clearInterval(this.timerIntervalId);
     clearTimeout(this.recordingTimeoutId);
     this.mediaRecorder.stop();
-    if (callback) {
-      callback();
-    }
-  }
 
-  // cancelRecording() {
-  //   clearInterval(this.recordingIntervalId);
-  //   clearTimeout(this.recordingTimeoutId);
-  //   this.stopMediaRecorder();
-  //   this.chunks = [];
-  //   this.recordBtn.value = "Ready";
-  //   this.recordBtn.dataset.state = "ready";
-  //   this.countdownDiv.classList.add("hidden");
-  //   this.timerDiv.classList.add("hidden");
-  //   this.cancelBtn.style.display = "none";
-  // }
+    this.recordBtn.value = "Ready";
+    this.recordBtn.disabled = true;
+  }
 }
 
 if (window.location.pathname === "/main") {
